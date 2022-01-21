27272a85dc9ac64b72867c748bf209315535a8b6
Oscar Cunningham <mail@oscarcunningham.com>
2022-01-21 18:32:51 +0000

    Write bytes to pickle file

--- src/LLS_files.py
+++ src/LLS_files.py
@@ -27,14 +27,14 @@ def append_to_file_from_string(file_name, input_string, indent = 0, verbosity = 
 def file_from_object(file_name, input_object, indent = 0, verbosity = 0):
     """Write object to file"""
     print_message('Writing file "' + file_name + '" ...', 3, indent = indent, verbosity = verbosity)
-    with open(file_name, "w") as output_file:
+    with open(file_name, "wb") as output_file:
         pickle.dump(input_object, output_file)
     print_message('Done\n', 3, indent = indent, verbosity = verbosity)
 
 def object_from_file(file_name, indent=0, verbosity=0):
     """Load object from file"""
     print_message('Reading file "' + file_name + '" ...', 3, indent = indent, verbosity = verbosity)
-    with open(file_name, "r") as object_file:
+    with open(file_name, "rb") as object_file:
         input_object = pickle.load(object_file)
     print_message('Done\n', 3, indent = indent, verbosity = verbosity)
     return input_object
